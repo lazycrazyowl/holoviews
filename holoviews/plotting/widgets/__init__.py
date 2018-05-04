93ca7c8fa1e7de8ce372e33d6fdd21585b585e41
Philipp Rudiger <P.Rudiger@ed.ac.uk>
2018-05-04 18:38:45 +0100

    Sort dynamic dimension values in widget constructor

--- holoviews/plotting/widgets/__init__.py
+++ holoviews/plotting/widgets/__init__.py
@@ -124,9 +124,15 @@ class NdWidget(param.Parameterized):
             self.renderer = Store.renderers[backend]
         else:
             self.renderer = renderer
+
         # Create mock NdMapping to hold the common dimensions and keys
+        sorted_dims = []
+        for dim in self.dimensions:
+            if dim.values and all(isnumeric(v) for v in dim.values):
+                dim = dim.clone(values=sorted(dim.values))
+            sorted_dims.append(dim)
         self.mock_obj = NdMapping([(k, None) for k in self.keys],
-                                  kdims=list(self.dimensions), sort=False)
+                                  kdims=sorted_dims, sort=False)
 
         NdWidget.widgets[self.id] = self
 
@@ -378,7 +384,7 @@ class SelectionWidget(NdWidget):
                 # Widgets currently detect dynamic mode by type
                 # this value representation is now redundant
                 # and should be removed in a refactor
-                values = sorted(dim.values)
+                values = dim.values
                 dim_vals = {i: i for i, v in enumerate(values)}
                 widget_type = 'slider'
                 value_labels = escape_list(escape_vals([dim.pprint_value(v)
